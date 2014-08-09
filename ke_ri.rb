70d61b143d6ba6afeccb5e5f87b60bcdc08e4d26
David Kuo <s50407s@gmail.com>
2014-08-10 01:28:21 +0800

    Classify KeKeRi

--- ke_ri.rb
+++ ke_ri.rb
@@ -3,239 +3,168 @@ require './plurk.rb'
 require './setting.rb'
 require 'time'
 require 'net/http'
-#Setup OAuth client by create a instance of Plurk class
-puts @setting = Setting.new #read setting file
-$plurk = Plurk.new(@setting.api_key, @setting.api_secret)
-$plurk.authorize(@setting.token_key, @setting.token_secret)
 
-$prevent_flag = true
+class KeKeRi
+	REGEXP_CHANNEL = /^CometChannel\.scriptCallback\((.*)\);/
+	attr_reader :setting
 
-def getChannel
-	begin
-	resp = $plurk.post("/APP/Realtime/getUserChannel")
-	rescue Timeout::Error
-		sleep 2 # let it take a rest
-		retry
+	def initialize
+		@setting = Setting.new
+		@plurkApi = Plurk.new(@setting.api_key, @setting.api_secret)
+		@plurkApi.authorize(@setting.token_key, @setting.token_secret)
+
+		getChannel while @channelUri.nil?
 	end
-	if resp["comet_server"].nil?
-		puts 'Failed to get channel.'
-		return false
-	end
-	$channelUri = resp["comet_server"]
-	$channelName = resp["channel_name"]
-	$channelOffset = -1
-	puts 'Get channel uri: ' + $channelUri
-	puts 'Get channel name: ' + $channelName
-	return true
-end
 
-getChannel while $channelUri.nil?
-
-REGEXP_CHANNEL = /^CometChannel\.scriptCallback\((.*)\);/
-def listenChannel
-	getChannel if $channelOffset == -3
-	params = { :channel => $channelName, :offset => $channelOffset }
-	params = params.map {|k,v| "#{k}=#{v}" }.join('&')
-	uri = URI.parse($channelUri + "?" + params)
-	http = Net::HTTP.new(uri.host, uri.port)
-	http.read_timeout = 170
-	retryGetting = 0
-	begin
-	res = http.start { |h|
-			h.get(uri.path+"?"+params)
-	}
-	rescue
-		retryGetting += 1
-		sleep 3
-		if retryGetting == 5
-			getChannel
-			return false
-		end
-		retry
-	end
-	res = REGEXP_CHANNEL.match res.body
-	json = JSON.parse res[1]
-	
-	readed = []
-	$channelOffset = json["new_offset"].to_i
-	return if json["data"].nil?
-
-	puts json["data"]
-	json["data"].each do |plurk|
-		case plurk["type"]
-		when "new_response"
-		when "new_plurk"
-			if plurk["owner_id"] == 5845208
-				next unless checkresponse plurk["plurk_id"]
-				responsePlurk(plurk["plurk_id"],"ㄎ__ㄖ")
-				$plurk.post('/APP/Timeline/mutePlurks', ids: plurk["plurk_id"])
-			elsif plurk["content"].match /ㄎ[_＿]*ㄖ/
-				next unless checkresponse plurk["plurk_id"]
-				responsePlurk(plurk["plurk_id"],"ㄎ__ㄖ")
-				$plurk.post('/APP/Timeline/mutePlurks', ids: plurk["plurk_id"])
-			end
-		end
-	end
-end
-
-def addPlurk(t,p)
-	if p==nil
-		ti = Time.now
-		json = nil
+	def listenChannel
+		getChannel if @channelOffset == -3
+		params = { :channel => @channelName, :offset => @channelOffset }
+		params = params.map { |k,v| "#{k}=#{v}" }.join('&')
+		uri = URI.parse(@channelUri + "?" + params)
+		http = Net::HTTP.new(uri.host, uri.port)
+		http.read_timeout = 170
+		retryGetting = 0
 		begin
-			$prevent_flag = false
-			json = $plurk.post('/APP/Timeline/plurkAdd', {:content=>t.to_s, :qualifier=>':'})
-			$prevent_flag = true
-		rescue
-			ti = Time.now
-			s = ti.to_s + "addplurk has error" + "\n" + $!.to_s
-			print s + "\n"
-			recordError s
-			if json != nil
-				if json.key?("error_text")
-					if json["error_text"] == "anti-flood-too-many-new"
-						ti = Time.now
-						s = ti.to_s + "too many new"
-						print s + "\n"
-						recordError(s)
-						
-						
-					end
-					return json
-				end						
+			res = http.start do |h|
+				h.get(uri.path+"?"+params)
 			end
-			ti = Time.now
-			s = ti.to_s + "addplurk will retry in 120 secend"+"\n"
-			print s + "\n"
-			recordError(s)
+		rescue
+			retryGetting += 1
+			sleep 3
+			if retryGetting == 5
+				getChannel
+				return false
+			end
+			retry
+		end
+		res = REGEXP_CHANNEL.match res.body
+		json = JSON.parse res[1]
+
+		readed = []
+		@channelOffset = json["new_offset"].to_i
+		return if json["data"].nil?
+
+		json["data"].each do |plurk|
+			case plurk["type"]
+			when "new_response"
+			when "new_plurk"
+				if plurk["owner_id"] == 5845208
+					next unless responsed? plurk["plurk_id"]
+					responsePlurk plurk["plurk_id"], "ㄎ__ㄖ"
+					@plurkApi.post '/APP/Timeline/mutePlurks', ids: [plurk["plurk_id"]]
+				elsif plurk["content"].match /ㄎ[_＿]*ㄖ/
+					next unless responsed? plurk["plurk_id"]
+					responsePlurk plurk["plurk_id"], "ㄎ__ㄖ"
+					@plurkApi.post '/APP/Timeline/mutePlurks', ids: [plurk["plurk_id"]]
+				end
+			end
+		end
+	end
+
+	def addPlurk(content)
+		begin
+			json = @plurkApi.post '/APP/Timeline/plurkAdd', content: content, qualifier: ':'
+		rescue
+			str = %(#{Time.now.to_s} [ERROR] Adding plurk has error: #{$!.to_s})
+			if json
+				str << "(#{json["error_text"]})" if json.key? "error_text"
+			end
+			puts str
 			sleep 120
 			retry
 		end
-		ti = Time.now
-		p ti.to_s + " addplurk"
-		print "\n"
-		p t
-		print "\n"
-		p json
-		print "\n"
 		return json
-		
-	else
-		addprivatePlurk(t,p)
 	end
-	
-end
 
-def addprivatePlurk(t,user_id)
-	
-	ti = Time.now 
-	begin
-		json = $plurk.post('/APP/Timeline/plurkAdd', {:content=>t.to_s, :qualifier=>':',:limited_to => [user_id]})
-	rescue
-		print ti.to_s + "add private plurk ha error" + "\n" + $!.to_s + "\n" 
-	end
-	
-	print ti.to_s + "addprivateplurk"
-	print "\n"
-	p json
-	print "\n"
-	
-	
-end
-
-def responsePlurk(plurkid,text)
-	
-	t = Time.now
-	begin
-		res = $plurk.post('/APP/Responses/responseAdd',{:plurk_id=>plurkid , :content=>text , :qualifier=>':'})
-	rescue
-		print t.to_s + "response plurk ha error" + "\n" + $!.to_s + "\n"
-	ensure
-		p t.to_s + "addres"+"\n"
-		p text
-		print"\n"
-		p res
-		print "\n"
-	end
-end
-
-def getUnreadPlurk
-	
-	begin 
-		json = $plurk.post('/APP/Timeline/getUnreadPlurks')
-	rescue
-		print "get unread plurk has error" + "\n" + $!.to_s + "\n"
-		sleep 5
-		retry
-	end
-	
-	return json
-end
-
-
-def checkresponse(plurkid)
-	
-	json = nil
-	begin
-		json = $plurk.post('/APP/Responses/get',{:plurk_id=>plurkid})
-	rescue
-		print "get response has error"+"\n" + $!.to_s + "\n"
-		sleep 5
-		retry
-	end
-	i = true
-	json["responses"].each{|res|
-	
-		if res["user_id"]==10428113 && res["content_raw"]=="ㄎ__ㄖ"  #<--- 10428113 robot id
-			i = false	
-		end
-	}
-	return i
-
-end
-
-def checkUnreadPlurk
-	json = $plurk.post('/APP/Timeline/getUnreadPlurks', limit: 20)
-	return if json["plurks"].nil?
-
-	json["plurks"].each do |plurk|
-		if plurk["owner_id"] == 5845208
-			next unless checkresponse plurk["plurk_id"]
-			responsePlurk(plurk["plurk_id"], "ㄎ__ㄖ")
-			$plurk.post('/APP/Timeline/mutePlurks', ids: plurk["plurk_id"])
-		elsif plurk["content"].match /ㄎ[_＿]*ㄖ/
-			next unless checkresponse plurk["plurk_id"]
-			responsePlurk(plurk["plurk_id"], "ㄎ__ㄖ")
-			$plurk.post('/APP/Timeline/mutePlurks', ids: plurk["plurk_id"])
+	def addPrivatePlurk(content, user_id)
+		begin
+			json = @plurkApi.post '/APP/Timeline/plurkAdd', content: content, qualifier: ':', limited_to: [user_id]
+		rescue
+			str = %(#{Time.now.to_s} [ERROR] Adding private plurk has error: #{$!.to_s})
+			if json
+				str << "(#{json["error_text"]})" if json.key? "error_text"
+			end
+			puts str
+			sleep 120
+			retry
 		end
 	end
-end
 
-def checkAlerts  # not finish
-	
-	alert = $plurk.post('/APP/Alerts/getActive')
-	
-	alert.each{|ale|
-		print ale
-		print "\n"
-		print "------------------------"
-		print "\n"
-	}
-	
-end
+	def responsePlurk(plurk_id, content)		
+		begin
+			res = @plurkApi.post '/APP/Responses/responseAdd', plurk_id: plurk_id, content: content, qualifier: ':'
+		rescue
+			puts %(#{Time.now.to_s} [ERROR] Responsing plurk has error: #{$!.to_s})
+		end
+	end
 
-def recordError(text)
-	begin
-		re = File.open("./record","a+")
-		re.write(text + "\n")
-		re.write("==============================="+"\n")
-		re.close
-	rescue
-		print "record file has error" + $!.to_s + "\n"
+	def checkUnreadPlurk
+		json = @plurkApi.post '/APP/Timeline/getUnreadPlurks', limit: 20
+		return if json["plurks"].nil?
+
+		json["plurks"].each do |plurk|
+			if plurk["owner_id"] == 5845208
+				next unless responsed? plurk["plurk_id"]
+				responsePlurk plurk["plurk_id"], "ㄎ__ㄖ"
+				@plurkApi.post '/APP/Timeline/mutePlurks', ids: [plurk["plurk_id"]]
+			elsif plurk["content"].match /ㄎ[_＿]*ㄖ/
+				next unless responsed? plurk["plurk_id"]
+				responsePlurk plurk["plurk_id"], "ㄎ__ㄖ"
+				@plurkApi.post '/APP/Timeline/mutePlurks', ids: [plurk["plurk_id"]]
+			end
+		end
+	end
+
+	private
+
+	def getUnreadPlurk
+		begin 
+			json = @plurkApi.post '/APP/Timeline/getUnreadPlurks'
+		rescue
+			puts %(#{Time.now.to_s} [ERROR] Getting unread plurks has error: #{$!.to_s})
+			sleep 5
+			retry
+		end
+
+		return json
+	end
+
+	def responsed?(plurk_id)
+		begin
+			json = @plurkApi.post '/APP/Responses/get', plurk_id: plurk_id
+		rescue
+			puts %(#{Time.now.to_s} [ERROR] Getting response has error: #{$!.to_s})
+			sleep 5
+			retry
+		end
+		json["responses"].each do |res|
+			return false if res["user_id"] == 10428113 && res["content_raw"] == "ㄎ__ㄖ"  #<--- 10428113 robot id
+		end
+		true
+	end
+
+	def getChannel
+		begin
+			resp = @plurkApi.post("/APP/Realtime/getUserChannel")
+		rescue Timeout::Error
+			sleep 2 # let it take a rest
+			retry
+		end
+		if resp["comet_server"].nil?
+			puts 'Failed to get channel.'
+			return false
+		end
+		@channelUri = resp["comet_server"]
+		@channelName = resp["channel_name"]
+		@channelOffset = -1
+		puts 'Get channel uri: ' + @channelUri
+		puts 'Get channel name: ' + @channelName
+		return true
 	end
 end
 
-print "ke ke ri robot start"+"\n"
+instance = KeKeRi.new
+puts "Ke Ke Ri Robot started."
 
 # Thread.new {
 #   while true
@@ -249,27 +178,27 @@ print "ke ke ri robot start"+"\n"
 #   end
 # }
 Thread.new {
-  begin
-    listenChannel while true
-  rescue
-    retry
-  end
+	begin
+		instance.listenChannel while true
+	rescue
+		retry
+	end
 }
 
 # check unreadPlurk once on start
 begin
-	checkUnreadPlurk
+	instance.checkUnreadPlurk
 rescue
-	print Time.now.to_s + "checkcmd has errer" + "\n" + $!.to_s + "\n"
+	puts %(#{Time.now.to_s} [ERROR] Checking unread plurk has error: #{$!.to_s})
 end
 
 while true
 	case gets.chomp
-		when "check"
-			p checkUnreadPlurk
-		when "get"
-			p getUnreadPlurk
-		when "close"
-			break
+	when "check"
+		p instance.checkUnreadPlurk
+	when "get"
+		p instance.getUnreadPlurk
+	when "close"
+		exit
 	end
 end
